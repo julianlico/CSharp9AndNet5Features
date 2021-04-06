8fa7712e96c484aa8f0f23bed2bb80ffd23d8909
jlico <jlico@ecri.org>
2021-04-06 12:38:04 -0400

    Added Covariant return types when overriding methods

--- Person.cs
+++ Person.cs
@@ -10,5 +10,20 @@
             FirstName = firstName;
             LastName = lastName;
         }
+
+        public Person() { }
+
+        public virtual Person Clone() => new Person() { FirstName = FirstName, LastName = LastName };
+    }
+
+    public class Student : Person
+    {
+        public string ID;
+        // Covariant return types
+        // In C# 9 you can override the Person Clone method to return a more specific "Student" instead of Person Type
+        // Old Way
+        //public override Person Clone() => new Student { FirstName = FirstName, LastName = LastName, ID = ID };
+        // New Way
+        public override Student Clone() => new Student { FirstName = FirstName, LastName = LastName, ID = ID };
     }
 }
